17102a06c01a0c2a842214e2f47763ed1689de9a
Kapil Kashyap <kapil.kashyaps@gmail.com>
2023-11-11 01:28:30 +0530

    fix(#12): Double tapping on mobile zooms the browser
    Added an event listener to avoid zooming browsers on mobile devices
    
    - Updated minified script file
    
    Fixes: #12

--- src/script.js
+++ src/script.js
@@ -93,6 +93,19 @@ import predefinedMazeCoordinates from '../assets/data/predefined-maze-coordinate
 				document.body.style.zoom = '200%';
 			}
 			document.body.classList.add('portable-device');
+			// Avoiding double-tap zooming on mobile devices
+			// Inspired by https://gist.github.com/ethanny2/44d5ad69970596e96e0b48139b89154b
+			document.body.addEventListener('touchend', (function() {
+				let lastTap = 0;
+				return function(event) {
+					const curTime = new Date().getTime();
+					const tapLen = curTime - lastTap;
+					if (tapLen < 500 && tapLen > 0) {
+						event.preventDefault();
+					}
+					lastTap = curTime;
+				};
+			})());
 			return true;
 		}
 		return false;
